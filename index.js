99b18493bce6274c3d4dd8e74aa9647a17259beb
Christopher Shaw <silvertn350@yahoo.com>
2022-09-27 13:50:07 -0500

    updated with attack ability

--- index.js
+++ index.js
@@ -10,20 +10,40 @@ c.fillRect(0,0, canvas.width, canvas.height)
 const gravity = 0.7
 
 class Sprite {
-    constructor({position,velocity}) {
+    constructor({position,velocity,color = 'red',offset}) {
         this.position = position
-        this.velocity = velocity 
+        this.velocity = velocity
+        this.width = 50
         this.height = 150
         this.lastKey
+        this.attackBox = {
+            position: {
+                x: this.position.x,
+                y: this.position.y
+            },
+            offset,
+            width: 100,
+            height: 50,
+        }
+        this.color = color
+        this.isAttacking
     }
 
     draw(){
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 50, this.height)
-    }
+        c.fillStyle = this.color
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+        //attack box is drawn here
+        if (this.isAttacking) {
+        c.fillStyle = 'green'
+        c.fillRect(this.attackBox.position.x, this.attackBox.position.y , this.attackBox.width, this.attackBox.height)
+    } 
+}
 
     update(){
         this.draw()
+        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
+        this.attackBox.position.y = this.position.y
 
         this.position.x += this.velocity.x
         this.position.y = this.position.y + this.velocity.y
@@ -32,6 +52,12 @@ class Sprite {
             this.velocity.y = 0
         } else this.velocity.y += gravity
     
+    }
+    attack() {
+        this.isAttacking = true
+        setTimeout(() => {
+            this.isAttacking = false
+        }, 100)
     }
 }
 
@@ -44,6 +70,10 @@ const player = new Sprite({
     velocity: {
         x: 0,
         y: 10
+    },
+    offset: {
+        x: 0,
+        y: 0
     }
 }) 
 
@@ -55,10 +85,14 @@ const enemy = new Sprite({
     velocity: {
         x:0,
         y: 0
+    },
+    color: 'blue',
+    offset: {
+        x: -50,
+        y: 0
     }
 }) 
 
-console.log(player)
 
 const keys = {
     a: {
@@ -74,6 +108,18 @@ const keys = {
         pressed: false
     }
 }
+
+
+function rectangularCollision({rectangle1,rectangle2}) {
+    return (
+        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= 
+        rectangle2.position.x && rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width && 
+        rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y
+        && rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height 
+    )
+}
+
+
 
 function animate() {
     window.requestAnimationFrame(animate)
@@ -99,6 +145,24 @@ function animate() {
         enemy.velocity.x = -5
     } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
         enemy.velocity.x = 5
+    }
+
+    //Detect for collision
+    if (rectangularCollision({
+        rectangle1: player,
+        rectangle2: enemy
+    }) &&
+        player.isAttacking)  {
+        player.isAttacking = false
+        console.log('player attacked')
+    }
+    if (rectangularCollision({
+        rectangle1: enemy,
+        rectangle2: player
+    }) &&
+        enemy.isAttacking)  {
+        enemy.isAttacking = false
+        console.log('enemy attacked')
     }
 }
 
@@ -126,6 +190,10 @@ window.addEventListener('keydown', (event) => {
             player.velocity.y = -20 
             player.lastKey = 'w'
             break
+            //space bar to attack !
+        case ' ':
+            player.attack()
+            break
         //Enemy Keys
         case 'ArrowRight':
             keys.ArrowRight.pressed = true
@@ -137,6 +205,9 @@ window.addEventListener('keydown', (event) => {
             break
         case 'ArrowUp':
             enemy.velocity.y = -20
+            break
+        case 'ArrowDown':
+            enemy.isAttacking = true
             break
     }
     console.log(event.key)
